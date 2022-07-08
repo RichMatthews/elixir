66701b3463171076c4d5aa0838a5eb13ec7185b5
Rich Matthews <richjmatt26@gmail.com>
2022-07-08 17:22:03 +0100

    refactor to include colors

--- wordle/lib/wordle.ex
+++ wordle/lib/wordle.ex
@@ -1,8 +1,5 @@
 # based on the popular game Wordle: https://www.nytimes.com/games/wordle/index.html
 # In this version the following indicates the letter guessed
-# "_" indicates the letter is not in the word
-# "p" indicates a p is in the word but not in the correct position
-# "P" indicates a p is in the word and is in the correct position
 
 defmodule RandomWord do
   @random_words [
@@ -34,7 +31,7 @@ defmodule RandomWord do
   ]
 
   def generate_random_word() do
-    Enum.random(@random_words) |> String.split("", trim: true)
+    Enum.random(@random_words) |> String.upcase() |> String.split("", trim: true)
   end
 end
 
@@ -44,9 +41,14 @@ defmodule Wordle do
     # IO.inspect("Remaining letters: #{letters}")
 
     guess =
-      IO.gets("What is your guess? (#{total_guesses + 1})\n")
+      IO.gets("")
+      |> String.slice(0..4)
       |> String.trim()
+      |> String.upcase()
       |> String.split("", trim: true)
+      |> Enum.with_index(fn letter, index ->
+        %{color: "grey", letter: letter, index: index}
+      end)
 
     total_guesses = total_guesses + 1
     guess = check_guess(guess, word_of_the_day)
@@ -54,21 +56,47 @@ defmodule Wordle do
   end
 
   def check_guess(guess, word_of_the_day) do
-    guess_with_correct_letters = determine_correct_letters_in_guess(guess, word_of_the_day)
+    guess_with_correct_letters =
+      Enum.map(guess, fn x -> check_letter_in_correct_position(x, word_of_the_day) end)
 
-    Enum.zip(guess_with_correct_letters, [0, 1, 2, 3, 4])
-    |> Enum.map(fn x -> check_letter_in_word(x, guess_with_correct_letters, word_of_the_day) end)
-    |> IO.inspect()
+    guess_with_letters_in_word =
+      Enum.map(guess_with_correct_letters, fn x ->
+        check_letter_in_word(x, guess_with_correct_letters, word_of_the_day)
+      end)
+
+    Enum.each(guess_with_letters_in_word, fn x -> write_with_color(x) end)
+    IO.puts("")
+    guess_with_letters_in_word
   end
 
-  def determine_correct_letters_in_guess(guess, word_of_the_day) do
-    Enum.zip(guess, [0, 1, 2, 3, 4])
-    |> Enum.map(fn x -> check_letter_in_correct_position(x, word_of_the_day) end)
+  def write_with_color(%{color: color, letter: letter}) do
+    if color == "green" do
+      IO.write("#{letter} " |> Colors.green() |> Colors.bold())
+    else
+      if color == "orange" do
+        IO.write("#{letter} " |> Colors.magenta() |> Colors.bold())
+      else
+        IO.write("#{letter} " |> Colors.black() |> Colors.bold())
+      end
+    end
+  end
+
+  def check_letter_in_correct_position(
+        %{letter: letter, index: index},
+        word_of_the_day
+      ) do
+    corresponding_letter_of_index_to_check = Enum.at(word_of_the_day, index)
+
+    if corresponding_letter_of_index_to_check == letter do
+      %{color: "green", letter: letter, index: index}
+    else
+      %{color: "grey", letter: letter, index: index}
+    end
   end
 
   def check_if_guess_correct(guess, total_guesses, word_of_the_day) do
-    if String.downcase(Enum.join(guess)) == Enum.join(word_of_the_day) do
-      IO.inspect(
+    if Enum.all?(guess, fn x -> x.color == "green" end) do
+      IO.puts(
         "You won! The correct answer was #{word_of_the_day}. You got it in #{total_guesses} guesses"
       )
     else
@@ -76,35 +104,25 @@ defmodule Wordle do
     end
   end
 
-  def check_if_guesses_remain(total_guesses, _) when total_guesses == 6 do
-    IO.inspect("bad luck out of guesses")
+  def check_if_guesses_remain(total_guesses, word_of_the_day) when total_guesses == 6 do
+    IO.inspect("bad luck out of guesses, the word was #{word_of_the_day}")
   end
 
   def check_if_guesses_remain(total_guesses, word_of_the_day) do
     play(total_guesses, word_of_the_day)
   end
 
-  def check_letter_in_correct_position({letter, index}, word_of_the_day) do
-    corresponding_letter_of_index_to_check = Enum.at(word_of_the_day, index)
-
-    if corresponding_letter_of_index_to_check == letter do
-      String.upcase(letter)
-    else
-      String.downcase(letter)
-    end
-  end
-
   def get_previous_portion_of_word(index, _) when index == 0 do
-    ""
+    []
   end
 
   def get_previous_portion_of_word(index, guess) do
-    String.slice(Enum.join(guess), 0..index)
+    Enum.filter(guess, fn x -> x.index <= index end)
   end
 
-  def check_letter_in_word({letter, index}, guess, word_of_the_day) do
-    if letter == String.upcase(letter) do
-      letter
+  def check_letter_in_word(%{color: color, letter: letter, index: index}, guess, word_of_the_day) do
+    if color == "green" do
+      %{color: "green", letter: letter, index: index}
     else
       if Enum.member?(word_of_the_day, letter) do
         occurences_of_letter_in_word = Enum.filter(word_of_the_day, fn x -> x == letter end)
@@ -113,27 +131,27 @@ defmodule Wordle do
 
         letter_already_occured =
           previous_portion_of_word
-          |> String.split("", trim: true)
           |> Enum.filter(fn x -> x == letter end)
-          |> Kernel.length() >= 0
+          |> length >= 0
 
-        letter_already_covered =
-          letter_already_occured == Kernel.length(occurences_of_letter_in_word)
+        letter_already_covered = letter_already_occured == length(occurences_of_letter_in_word)
 
         all_occurences_are_already_correct =
           List.delete_at(guess, index)
-          |> Enum.filter(fn x -> letter == String.downcase(x) end)
-          |> Enum.all?(fn x -> String.upcase(x) == x end)
+          |> Enum.filter(fn x -> letter == x.letter end)
+          |> Enum.all?(fn x -> x.color == "green" end)
 
         if letter_already_covered or
              (all_occurences_are_already_correct and
-                Kernel.length(Enum.filter(guess, fn x -> letter == String.downcase(x) end)) > 1) do
-          "_"
+                Kernel.length(Enum.filter(guess, fn x -> letter == String.downcase(x.letter) end)) >
+                  1) do
+          %{color: "grey", letter: letter, index: index}
         else
-          letter
         end
+
+        %{color: "orange", letter: letter, index: index}
       else
-        "_"
+        %{color: "grey", letter: letter, index: index}
       end
     end
   end
